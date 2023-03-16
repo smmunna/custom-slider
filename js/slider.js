1d3914a6b52f455654153e68a48ba71916584b77
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-16 23:34:17 +0600

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,19 @@
 console.log('Slider Connected')
+let images =[
+    'images/img-1.jpg',
+    'images/img-2.jpg',
+    'images/img-3.jpg',
+    'images/img-4.jpg',
+    'images/img-5.jpg',
+]
+let counter =0;
+let image = document.getElementById('image');
+setInterval(()=>{
+    if(counter === images.length){
+        counter=0;
+    }
+    image.setAttribute('src',images[counter]);
+    //console.log(images[counter]);
+    counter++;
+    
+},1000)
